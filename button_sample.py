6d1d2b0d6089bef20ab6746f171815ad9381f8b4
Jetson User <jetson-rw@mail2you.net>
2020-09-03 22:55:55 +0800

    GPIO button works

--- button_sample.py
+++ button_sample.py
@@ -1,43 +1,56 @@
 import Jetson.GPIO as GPIO
 import time
+from subprocess import call
 
+shutdown_pin = 21  # Board pin 40
 
-
-shutdown_pin = 21  # Board pin 18
 
 def button_callback(channel):
-    print("Button was pushed, shutdown now!")
-    from subprocess import call
-    call("echo asdfQWER | sudo -S shutdown -h now", shell=True)    
 
+    print("Button was pushed!")    
+    #sample for calling shell commands
+    #call("echo MYPASSWORD | sudo -S shutdown -h now", shell=True) 
+    call("uptime ", shell=True)    
+    GPIO.remove_event_detect(channel)
     
+
+def setup_button(channel):
+    print("{}: RESET GPIO {} for push".format(time.time(), channel ) )
+    GPIO.cleanup(channel)
+    GPIO.remove_event_detect(channel)
+    GPIO.setup(channel, GPIO.OUT) 
+    GPIO.output(channel, GPIO.LOW)    
+    GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+    #can use GPIO.RISING / FALLING / BOTH
+    GPIO.add_event_detect(channel, GPIO.RISING, callback=button_callback, bouncetime=300) # Setup event on pin 10 rising edge          
+
+
 def main():
-    # Pin Setup:
-    #GPIO.cleanup()
-
-
     GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
+    #GPIO.setwarnings(False)
     GPIO.remove_event_detect(shutdown_pin)
 
     print("Starting demo now! Press CTRL+C to exit")
-
-    
+  
+  
     try: 
-        print("Setting Shutdown to LOW")
-        GPIO.setup(shutdown_pin, GPIO.OUT) 
-        GPIO.output(shutdown_pin, GPIO.LOW)
-        ##time.sleep(1)
-        GPIO.cleanup(shutdown_pin)
-        GPIO.setup(shutdown_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-        GPIO.add_event_detect(shutdown_pin, GPIO.BOTH, callback=button_callback) # Setup event on pin 10 rising edge          
 
+        while True:
+            setup_button(shutdown_pin)
+            print ('.')
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        print ("Ctrl-C")
+        GPIO.remove_event_detect(shutdown_pin)
+    except:
+        print ("Other error or exception occurred!" )
+        GPIO.remove_event_detect(shutdown_pin)
     finally:
-        pass
+        print ("GPIO Cleanup" )
+        GPIO.cleanup()
 
-    
-    time.sleep(10)
-    GPIO.remove_event_detect(shutdown_pin)
+
 
 
 if __name__ == '__main__':
